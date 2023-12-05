d6546703ed6497ac24251bdd1a57a1bafdb8682c
Nazmiee18 <mfirdaus.nz1819@gmail.com>
2023-12-05 22:36:39 +0700

    fix error mongodb

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const db_url = process.env.db_url || 'mongodb:localhost/my_app';
+const db_url = process.env.db_url || 'mongodb+srv://mfirdausnz1819:firdaus1819@cluster0.qv7lqu6.mongodb.net/mata_pembelajaran';
 
 const db = mongoose.connect(db_url);
 
